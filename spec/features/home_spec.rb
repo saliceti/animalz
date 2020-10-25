2aef76062047f47792bead2fe76b7aae4e481de0
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-10-25 10:59:01 +0000

    Logo and basic menu

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,6 +4,7 @@ feature 'Home' do
   scenario 'Visit' do
     when_a_user_visits_the_home_page
     then_they_receive_greeting_message
+    and_the_menus_are_displayed
   end
 end
 
@@ -13,4 +14,8 @@ end
 
 def then_they_receive_greeting_message
   expect(page).to have_text('Welcome to our planet!')
+end
+
+def and_the_menus_are_displayed
+  expect(page).to have_link('Home', href: root_path)
 end
