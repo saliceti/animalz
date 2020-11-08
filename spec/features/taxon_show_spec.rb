49e1c29c1b0456e8369bc469af8d51560fec5ce5
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-08 13:35:40 +0000

    Display child taxons on the show taxon page

--- spec/features/taxon_show_spec.rb
+++ spec/features/taxon_show_spec.rb
@@ -11,6 +11,12 @@ feature 'Taxon show' do
     when_the_user_visits_a_species_page
     then_the_full_classification_is_displayed
   end
+  scenario 'displays child taxons' do
+    given_a_full_taxon_hierarchy
+    and_a_family_with_2_subfamilies_and_1_genus_children
+    when_the_user_visits_the_family_page
+    then_the_2_child_taxons_are_displayed
+  end
 end
 
 def when_the_user_visits_a_species_page
@@ -26,4 +32,30 @@ def then_the_full_classification_is_displayed
     taxon = Taxon.where(rank: rank).first
     expect(page).to have_link taxon.scientific_name, href: taxon_path(taxon)
   }
+end
+
+def and_a_family_with_2_subfamilies_and_1_genus_children
+  @family = Taxon.where(rank: 'Family').first
+  @subfamily2 = Taxon.create(
+    rank: 'Subfamily', parent: @family,
+    common_name: 'Subfamily common name 2', scientific_name: 'Subfamily scientific name 2'
+  )
+  @subfamily2.save
+  @genus2 = Taxon.create(
+    rank: 'Genus', parent: @family,
+    common_name: 'Genus common name 2', scientific_name: 'Genus scientific name 2'
+  )
+  @genus2.save
+end
+
+def when_the_user_visits_the_family_page
+  visit taxon_path(@family)
+end
+
+def then_the_2_child_taxons_are_displayed
+  expect(page).to have_text "Child taxons"
+  subfamily1 = Taxon.where(scientific_name: 'Subfamily scientific name').first
+  [subfamily1, @subfamily2, @genus2].each do |taxon|
+    expect(page).to have_link taxon.common_name, href: taxon_path(taxon)
+  end
 end
