4082e89d67ea2b966f8f44ccd95f4a3af98f2a7b
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2020-11-08 17:12:10 +0000

    Create child taxon

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -1,6 +1,6 @@
 class TaxonsController < ApplicationController
   before_action :set_taxon, only: [:show, :edit, :update, :destroy]
-  helper_method :embed_link
+  helper_method :embed_link, :parent_taxon
 
   def index
     if params[:rank]
@@ -18,7 +18,8 @@ class TaxonsController < ApplicationController
 
   def new
     @taxons = Taxon.all
-    @taxon = Taxon.new
+    @parent_taxon = Taxon.find(params[:readonly_parent_taxon]) if params[:readonly_parent_taxon]
+    @taxon = Taxon.new(parent: @parent_taxon)
   end
 
   def edit
@@ -54,6 +55,15 @@ class TaxonsController < ApplicationController
   end
 
   private
+
+  def parent_taxon(form)
+    if params[:readonly_parent_taxon]
+      buffer = form.hidden_field :parent_id, :value => @taxon.parent_id
+      buffer << helpers.link_to(@taxon.parent.common_name, taxon_path(@taxon.parent))
+    else
+      form.collection_select(:parent_id, @taxons, :id, :rank_and_common_name, :include_blank => "")
+    end
+  end
 
   def embed_link(youtube_id)
     "https://www.youtube.com/embed/#{youtube_id}"
--- spec/features/taxon_crud_spec.rb
+++ spec/features/taxon_crud_spec.rb
@@ -22,11 +22,17 @@ feature 'Taxon CRUD' do
     then_the_user_is_redirected_to_the_index
     and_the_taxon_is_not_there
   end
+  scenario 'Create child taxon' do
+    given_a_taxon_already_exists
+    when_a_user_clicks_new_child_taxon
+    then_the_form_is_prepopulated
+    and_a_the_child_taxon_can_be_created
+  end
 end
 
 def given_a_taxon_already_exists
-  chordata = Taxon.create(rank: 'Phylum', common_name: 'Chordates', scientific_name: 'Chordata')
-  chordata.save
+  @chordata = Taxon.create(rank: 'Phylum', common_name: 'Chordates', scientific_name: 'Chordata')
+  @chordata.save
   expect(Taxon.any?).to be true
 end
 
@@ -90,6 +96,26 @@ end
 
 def and_the_taxon_is_not_there
   expect(page).not_to have_text 'Chordates'
+end
+
+def when_a_user_clicks_new_child_taxon
+  visit taxon_path(@chordata)
+  expect(page).to have_link 'New child taxon', href: new_taxon_path(readonly_parent_taxon: @chordata)
+  click_link 'New child taxon'
+end
+
+def then_the_form_is_prepopulated
+  expect(page).to have_field('taxon_parent_id', type: 'hidden', with: @chordata.id)
+  expect(page).to have_link @chordata.common_name, href: taxon_path(@chordata)
+end
+
+def and_a_the_child_taxon_can_be_created
+  fill_in 'Common name', with: 'Amphibian'
+  fill_in 'Scientific name', with: 'Amphibia'
+  click_button 'commit'
+  expect(page).to have_text 'Taxon was successfully created'
+  expect(page).to have_text 'Common name: Amphibian'
+  expect(page).to have_text 'Scientific classification: Chordata > Amphibia'
 end
 
 private
