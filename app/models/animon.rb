4aac75eb0fb57d5b18b4af6092889ca6476ddbbc
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-01-30 16:16:16 +0000

    Animon accepts_nested_attributes_for taxon

--- app/models/animon.rb
+++ app/models/animon.rb
@@ -2,6 +2,7 @@ class Animon < ApplicationRecord
   belongs_to :taxon
   has_many :youtube_videos
   has_one_attached :picture
+  accepts_nested_attributes_for :taxon
 
   validates :taxon, presence: true
 
