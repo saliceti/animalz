2aff0a3dbfd4814b27bbf834b4d56bd06442f35d
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-03-27 16:10:22 +0000

    Disable XSS protection
    Temporary solution with simple embed code copy/paste is not secure. This
    should be reworked so XSS protection can be enabled again.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Animalz
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # TODO: remove XSS risk script with Getty image and revert to secure default header
+    config.action_dispatch.default_headers["X-XSS-Protection"] = "0"
   end
 end
