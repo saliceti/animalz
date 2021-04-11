797aabb428c24342b4e292bff26d6df2f6160ad5
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-04-11 20:33:45 +0100

    Validate URL

--- app/models/animon_link.rb
+++ app/models/animon_link.rb
@@ -1,4 +1,8 @@
 class AnimonLink < ApplicationRecord
   belongs_to :animon
   validates :url, presence: true
+
+  URL_REGEX = /\Ahttps:\/\/[\w-]+\.[\w\.\-\/]+\z/
+
+  validates_format_of :url, :with => URL_REGEX
 end
--- spec/models/animon_link_spec.rb
+++ spec/models/animon_link_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe AnimonLink, type: :model do
   it { is_expected.to have_db_column(:animon_id).of_type(:integer) }
   it { is_expected.to belong_to(:animon)}
   it { is_expected.to validate_presence_of(:url)}
+  it { is_expected.to allow_value('https://www.montclairlocal.news/2021/04/01/the-red-fox-montclairs-cutest-carnivore-whats-in-your-backyard/').for(:url) }
+  it { is_expected.to allow_value('https://www.montclairlocal.news/').for(:url) }
+  it { is_expected.to allow_value('https://www.montclairlocal.news').for(:url) }
+  it { is_expected.to allow_value('https://montclairlocal.news').for(:url) }
+  it { is_expected.not_to allow_value('www.montclairlocal.news/2021/04').for(:url) }
+  it { is_expected.not_to allow_value('https://wwwmontclairlocalnews/2021/04').for(:url) }
 end
